5da2e371e811ae3f28b2a4848a1e5106ecd8913b
thedaemoncoder <semajpa@gmail.com>
2017-04-20 11:49:06 +0200

    commit rango/urls.py

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -10,8 +10,10 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+#TEMPLATE_DIRS = ('<workspace>/tango_with_django_project/',)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -22,10 +24,9 @@ SECRET_KEY = 'noxx)+4kwd%&)(r7rmwx^h3esatgqn(s$6od9+**rqbsz45a&y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-TEMPLATE_DEBUG = True
+#TEMPLATE_DEBUG = True eh?
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -53,7 +54,6 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
@@ -77,9 +77,25 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
+)
+
+STATIC_PATH = os.path.join(BASE_DIR,'static')
+
+STATIC_URL = '/static/' # You may find this is already defined as such.
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Absolute path to the media directory
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 from django.contrib import admin
 
 urlpatterns = patterns('',
@@ -8,5 +9,18 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),  # added this to see rango's urls
-    url(r'^rango/about/', include('rango.urls')),  # added this to see rango's urls
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('django.views.static',
+        (r'^media/(?P<path>.*)',
+        'serve',
+        {'document_root': settings.MEDIA_ROOT}),
+                            )
+
+#print urlpatterns
+# At this point,
+#  urlpatterns = [<RegexURLResolver <RegexURLPattern list> (admin:admin) ^admin/>,
+#                 <RegexURLResolver <module 'rango.urls' from 'e:\code\tango_with_django_project\rango\urls.py'> (None:None) ^rango/>,
+#                  <RegexURLResolver <module 'rango.urls' from 'e:\code\tango_with_django_project\rango\urls.py'> (None:None) ^rango/about/>,
+#                  <RegexURLPattern None ^media/(?P<path>.*)>]
