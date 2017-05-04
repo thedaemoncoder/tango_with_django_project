be03e51129f03c4f5be32d78c2fc33ec74e261b1
thedaemoncoder <semajpa@gmail.com>
2017-05-04 22:58:49 +0200

    commit rango/urls.py

--- rango/views.py
+++ rango/views.py
@@ -207,7 +207,8 @@ def user_login(request):
         else:
             # Bad login details were provided. So we can't log the user in.
             print "Invalid login details: {0}, {1}".format(username, password)
-            return HttpResponse("Invalid login details supplied.")
+            response = "Invalid login details supplied. <br>"+" Username %s doesnt exist, or password is incorrect" % (username)
+            return HttpResponse(response)
 
     # The request is not a HTTP POST, so display the login form.
     # This scenario would most likely be a HTTP GET.
